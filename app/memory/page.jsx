cbfdb5de92186869ab25e29823b9db161485a5d1
matheussr1996-cmd <matheussr1996@gmail.com>
2023-10-18 00:09:21 -0300

    Update page.jsx

--- app/memory/page.jsx
+++ app/memory/page.jsx
@@ -1,11 +1,19 @@
-// start here
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import PageHeader from "../components/PageHeader"
+import PromptBox from "../components/PromptBox"
+import Title from "../components/Title"
+import TwoColumnLayout from "../components/TwoColumnLayout"
+import pageHeader from "../components/PageHeader"
+import ResultWithSources from "../components/ResultWithSources"
+import "../globals.css"
 
 const Memory = () => {
   return (
-    <div>
-      Memory
-    </div>
+    <Title
+      headingText={"Memory"}
+      emoji={"🧠"}
+    />
   )
 }
 
